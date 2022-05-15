2950aa9406225842bff20844c29abb8f8ff8805f
Jayeen28 <jayeen6783@gmail.com>
2022-05-15 22:11:52 +0600

    youtube link validation message triggered first before showing that another song is playing

--- src/commands/playMusic.js
+++ src/commands/playMusic.js
@@ -14,10 +14,10 @@ module.exports = {
     async execute(interaction) {
         const inputedData = interaction.options._hoistedOptions;
         const youtubeLink = inputedData[0].value;
-        if (playerInstance.playing) return interaction.reply({ content: 'There is already a song playing. Please stop the music first using "/controlmusic:stop" command.', ephemeral: true });
         const youtubeLinkRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?/;
         const youtubeLinkMatch = youtubeLink.match(youtubeLinkRegex);
         if (!youtubeLinkMatch) return interaction.reply({ content: 'Invalid youtube link.', ephemeral: true });
+        if (playerInstance.playing) return interaction.reply({ content: 'There is already a song playing. Please stop the music first using "/controlmusic:stop" command.', ephemeral: true });
         interaction.reply({ content: `Playing ${youtubeLinkMatch[0]}...` });
         videoToMP3(youtubeLinkMatch[0])
             .then(url => handleVC(interaction, url))
