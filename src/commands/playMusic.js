d329d8a96d530db133543c5b39928986d6632b5a
Jayeen28 <jayeen6783@gmail.com>
2022-05-18 01:02:40 +0600

    /playmusic command response changed

--- src/commands/playMusic.js
+++ src/commands/playMusic.js
@@ -14,11 +14,11 @@ module.exports = {
     async execute(interaction) {
         const inputedData = interaction.options._hoistedOptions;
         const youtubeLink = inputedData[0].value;
-        await interaction.reply({ content: `Command received.`, ephemeral: true });
+        await interaction.reply({ content: `Command received.` });
         const youtubeLinkRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?/;
         const youtubeLinkMatch = youtubeLink.match(youtubeLinkRegex);
         if (!youtubeLinkMatch) return await interaction.editReply({ content: `Invalid youtube link (${youtubeLink}).` });
-        if (playerInstance.playing) return await interaction.editReply({ content: 'There is already a song playing. Please stop the music first using "/controlmusic:stop" command.', ephemeral: true });
+        if (playerInstance.playing) return await interaction.editReply({ content: 'There is already a song playing. Please stop the music first using "/controlmusic:stop" command.' });
         await interaction.editReply({ content: `Downloading audio . . .` });
         const res = await videoToMP3(youtubeLinkMatch[0]);
         if (!res) return await interaction.editReply({ content: `Invalid youtube link (${youtubeLink}).` });
