ba84e7caab9f18b0e0805682c75c1ae75a69c903
jayeen28 <jayeen6783@gmail.com>
2022-08-21 18:28:03 +0600

    opensea floor price alert command registered

---
+++ src/commands/alert.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const saveAlert = require('../entities/opensea/saveAlert');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('alert')
+        .setDescription("The bot will alert you when the collection floor price goes equal to or under your specified price.")
+        .addStringOption(option =>
+            option.setName('collectionslug')
+                .setDescription('The collection slug of the collection.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName(`price`)
+                .setRequired(true)
+                .setDescription('Your desired price.')
+        ),
+    async execute(interaction) {
+        saveAlert(interaction);
+    }
+}
---
+++ src/entities/opensea/saveAlert.js
@@ -0,0 +1,8 @@
+module.exports = async (interaction) => {
+    try {
+        console.log(interaction)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
